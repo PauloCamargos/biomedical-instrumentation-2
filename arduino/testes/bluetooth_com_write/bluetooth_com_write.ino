738ced31868c79cb98989c96df48bb565c905d11
PauloCamargos <paulo.camargos@ufu.br>
2019-05-19 20:04:50 -0300

    adicionando filtro

--- arduino/testes/bluetooth_com_write/bluetooth_com_write.ino
+++ arduino/testes/bluetooth_com_write/bluetooth_com_write.ino
@@ -1,7 +1,9 @@
 #include "SoftwareSerial.h"
-#include "Statistics.h" 
+#include "Statistics.h"
 //https://github.com/provideyourown/statistics
 #include "pitches.h"
+
+#define fa 100 // frequencia de amostragem em hz
 
 #define TX 11 // pino tx
 #define RX 12 // pino rx
@@ -11,57 +13,62 @@
 #define NOTE NOTE_A7 // nota musical do buzzer
 
 // instanciando pino tx e rx do bluetooth
-SoftwareSerial blth(RX,TX);
+SoftwareSerial blth(RX, TX);
 
 // instanciando Statistics com 10 valores
 Statistics stat(50);
 int count = 0;
+float ta = (1.0 / fa) * 1000.0; // tempo de amostragem em ms
+unsigned long time_now = 0;
+
 void setup() {
   //inciando comunicacao no serial monitor e bluetooth
-  //Serial.begin(115200);
-  blth.begin(9600);
+  Serial.begin(115200);
+//  blth.begin(9600);
 
   // iniciando estatistica (stat)
   stat.reset();
 }
 
 void loop() {
-  // valor lido do tecido, entre 0-255
-  byte val = map(analogRead(ANALOG_PIN), 0,1023,0,255);
-  byte mappedVal = map(val,40,70,0,255);
-  
-  // incluindo o valor no array de stat
-  stat.addData(mappedVal);
+  float meanVal;
+  float stdDev;
+  // Quando o tecido eh distendido, a resistência cai
+  // A res cai a qual taxa?
+  // Exite um tempo de recuperação do tecido?
+  // Qual a durabilidade do tecido?
 
-  // media e desvio padrao
-  float meanVal = stat.mean();
-  float stdDev = stat.stdDeviation();
+  time_now = millis();
+  if (millis() < time_now + ta) {
 
-  int mappedMean = map(meanVal,110,190, 0,255);
+    // valor lido do tecido, entre 0-255
+    int val = analogRead(ANALOG_PIN);
+    //  byte mappedVal = map(val,40,70,0,255);
 
-  // soa alarme de desvio padrao < 40
-  if(stdDev <= 40){
+    // incluindo o valor no array de stat
+    stat.addData(val);
+
+    // media e desvio padrao
+    meanVal = stat.mean();
+    stdDev = stat.stdDeviation();
+
+    int mappedMean = map(meanVal, 0, 1023, 0, 255);
+
+    // soa alarme de desvio padrao < 40
+    if (stdDev <= 40) {
       triggerAlarm();
+    }
+      Serial.println(meanVal);
+//      Serial.print(","); 
+//      Serial.println(stdDev);
+
+//    blth.print(meanVal);
+//    blth.print("\n");
   }
 
-  //int mapped_value = map(mean_val, 40,80,0,255); 
-
-  // exibindo no serial monitor
-//  Serial.println("A " + String (mappedVal));
-//  Serial.println("L " + String (limite));
-
-//  Serial.println("S " + String (stdDev));
-//  Serial.println("M " + String (mean_val) + "\n----");
-
-//  Serial.println(val);
-  // enviando para smartphone
-  
-  blth.print(mappedVal);
-  blth.print("\n");
-  delay(80);
 }
 
-void triggerAlarm(){
+void triggerAlarm() {
   int noteDuration = 1000 / NOTE_FRACTION;
-  tone(13, NOTE ,noteDuration);
+  tone(13, NOTE , noteDuration);
 }
